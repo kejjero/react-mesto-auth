e9ba983e3dd377880b6fb9a0dc293b168f42f401
Maxim <kejerino@yandex.ru>
2022-05-23 23:06:45 +0800

    feat: added link for auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import logo from "../images/header__logo.svg"
+import {Link} from "react-router-dom";
 
-function Header() {
+function Header(isLoggedIn) {
+
+    const handleValue = isLoggedIn ? 'Регистрация' : 'Войти'
+    const handleLink = isLoggedIn ? '/sign-up' : '/sign-in'
+
     return (
-        <header className="header">
-            <img className={"header__logo"} src={logo} alt="Логотип"/>
+        <header className='header'>
+            <Link to={'/'} className='header__logo' src={logo} alt='Логотип'/>
+            <Link to={handleLink} className='header__enter' >{handleValue}</Link>
         </header>
     )
 }
