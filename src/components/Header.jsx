997a8baa95f76f049badcaf1803af2b5e3c9f01c
Maxim <kejerino@yandex.ru>
2022-05-25 23:49:13 +0800

    feat!: added styles for header and tooltip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import logo from "../images/header__logo.svg"
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
-function Header(isLoggedIn) {
-
-    const handleValue = isLoggedIn ? 'Регистрация' : 'Войти'
-    const handleLink = isLoggedIn ? '/sign-up' : '/sign-in'
+function Header(props) {
+    const location = useLocation();
 
     return (
         <header className='header'>
             <Link to={'/'} className='header__logo' src={logo} alt='Логотип'/>
-            <Link to={handleLink} className='header__enter' >{handleValue}</Link>
+            <nav className='header__wrapper'>
+                {props.isLoggedIn ? <p className='header__email'>{props.userEmail}</p> : <></>}
+                {props.isLoggedIn
+                    ? (
+                        <Link to="/sign-in"
+                              className="header__link header__link_logout"
+                              onClick={props.handleSignOut}>
+                            Выйти
+                        </Link>
+                    ) : (
+                        location.pathname === "/sign-in"
+                            ?
+                            <Link to="/sign-up" className="header__link">
+                                Регистрация
+                            </Link>
+                            :
+                            <Link to="/sign-in" className="header__link">
+                                Войти
+                            </Link>
+                    )}
+            </nav>
         </header>
     )
 }
