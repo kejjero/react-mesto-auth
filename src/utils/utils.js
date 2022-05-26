a6fc1efb25459cd8f131e4c77108a5b09e624b70
Maxim <kejerino@yandex.ru>
2022-05-26 19:56:38 +0800

    refactor: removed unnecessary variables

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,13 @@
 export const inputPerson = document.querySelector('.popup__input_person');
 export const inputAboutMe = document.querySelector('.popup__input_about-me');
-export const templateSelector = '#element-template';
 export const fromEditProfileSelector = '.popup__form_edit-profile';
-export const formAddPlaceSelector = '.popup__form_add-place';
 export const formEditAvatarSelector = '.popup__form_edit-avatar'
 export const imageSelector = '.popup_type_image';
 export const editProfileSelector = '.popup_type_edit-profile';
 export const addPlaceSelector = '.popup_type_add-place';
 export const editAvatarSelector = '.popup_type_edit-avatar'
 export const deleteCardSelector = '.popup_type_card-delete';
-export const listSelector = '.elements';
-export const profilePerson = '.profile__person';
 export const profileAboutMe = '.profile__about-me';
-export const profileAvatar = '.profile__avatar';
 export const userContext = {
     name: 'Имя',
     about: 'Деятельность',
